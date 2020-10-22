1129ec54628f55baa55b651a8f9962dbd589f5aa
Laurée <poussin.lauree@gmail.com>
2020-10-22 09:06:53 +0200

    update

--- Tamago/src/tamagotchi/Main.java
+++ Tamago/src/tamagotchi/Main.java
@@ -6,14 +6,14 @@ import menu.MenuInit;
 public class Main {
 	
 	// Init new Tamagotchi
-		public static Tamagotchi myTama = new Tamagotchi(0, 0, 0, 0, 0, 0, false, 0, 0, 0, false, 0, 0);
+	public static Tamagotchi myTama = new Tamagotchi(0, 0, 0, 0, 0, 0, false, 0, 0, 0, false, 0, 0);
 	
 	public static void main(String[] args) {
 
-		//MenuInit.startInit();
-		MyThread myThread = new MyThread();
-		myThread.start();
-		Game.displayMenu(myTama);
+		MenuInit.startInit(); // lancement du menu d'initialisation
+		MyThread myThread = new MyThread(); // création du Thread
+		myThread.start(); // lancement du Thread
+		Game.displayMenu(myTama); // lancement du jeu
 	}
 
 }
